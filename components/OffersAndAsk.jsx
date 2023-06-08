f16c59548fd9c6766850bfc37814ea83acbb7bfa
Balaganesh003 <k.balaganesh26@gmail.com>
2023-06-08 14:25:17 +0530

    Built the profile card

---
+++ components/OffersAndAsk.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OffersAndAsk = () => {
+  return <div className="w-full bg-red-400 h-48 rounded-lg"></div>;
+};
+
+export default OffersAndAsk;
--- pages/profile.js
+++ pages/profile.js
@@ -1,11 +1,95 @@
 import React from 'react';
+import ProfileBanner from '../assets/nova-profile-banner.png';
+import Image from 'next/image';
+import ProfilePhoto from '../assets/profile-photo.jpg';
+import PencileLogo from '../assets/PENCIL.svg';
+import OtherLogo from '../assets/svgexport-23.svg';
+import BatchLogo from '../assets/svgexport-24.svg';
+import DegreeLogo from '../assets/svgexport-25.svg';
+import OffersAndAsk from '../components/OffersAndAsk';
 
 const Profile = () => {
   return (
     <div className="flex flex-col md:flex-row md:gap-7 gap-3 lg:ml-[3.13rem] pt-[3.5rem] justify-center md:max-h-screen md:overflow-hidden  md:scrollbar-hide">
-      <div className="md:w-[388px] flex-shrink-0 md:ml-7 md:pt-11 md:pb-14 md:overflow-y-scroll md:max-h-screen bg-gradient-to-b md:scrollbar-hide">
-        <div className="w-full h-[200rem]  bg-gradient-to-b from-pink-600 to-blue-200"></div>
-        {/* Content for the left column */}
+      {/* Content for the left column */}
+      <div className="md:w-[400px] h-full flex-shrink-0 md:ml-7 md:pt-11 md:pb-14 md:overflow-y-scroll md:max-h-screen bg-gradient-to-b md:scrollbar-hide ">
+        <div className="w-full h-full">
+          <div className=" rounded-lg m-2 mb-3 shadow-lg">
+            {/* Profile Banner */}
+            <div className="w-full  h-48 rounded-t-lg">
+              <div className="rounded-t-lg relative">
+                <Image
+                  src={ProfileBanner}
+                  alt="Profile Banner"
+                  className="rounded-t-lg"
+                />
+                <div className="w-[150px] h-[150px] bg-white rounded-full absolute top-[18px] left-8 shadow-lg">
+                  <Image
+                    src={ProfilePhoto}
+                    alt="Profile Photo"
+                    className="w-full h-full rounded-full p-1"
+                  />
+                </div>
+                <div className="absolute top-[7.7rem] right-8 rounded-full hover:bg-[#eee] cursor-pointer  duration-200 transition-all">
+                  <Image
+                    src={PencileLogo}
+                    alt="Pencile Logo"
+                    className="w-[24px] h-[24px] "
+                  />
+                </div>
+              </div>
+            </div>
+            {/* Profile Details */}
+            <div className="w-full px-8 py-2 ">
+              <div className="flex justify-between gap-5 items-center">
+                <p className="flex text-2xl font-medium">
+                  Innovative Chnagemaker
+                </p>
+                <p className="flex text-xl text-gray-500">
+                  <span>(</span>
+                  <span>He/Him</span>
+                  <span>)</span>
+                </p>
+              </div>
+              <p className="text-xl pt-2">Building Hirable</p>
+              <p className="text-md pt-1 text-gray-500">@chnagemaker</p>
+            </div>
+            {/* Profile Stats */}
+            <div className="w-full px-8 py-7 ">
+              <div className="flex  gap-[16px] items-center">
+                <p className="flex items-center text-gray-500">
+                  <span className="font-medium text-black pr-1">1</span>post
+                </p>
+                <p className="flex items-center text-gray-500">
+                  <span className="font-medium text-black pr-1">3</span>comments
+                </p>
+                <p className="flex items-center text-gray-500">
+                  <span className="font-medium text-black pr-1">12</span>
+                  communities
+                </p>
+              </div>
+            </div>
+            {/* Profile Bio */}
+            <div className="w-full flex flex-col pt-3 gap-3 px-8 pb-8 ">
+              <div className="flex items-center ">
+                <Image src={OtherLogo} alt="Other Logo" />
+                <p className="pl-3 text-black">Other</p>
+              </div>
+              <div className="flex items-center ">
+                <Image src={BatchLogo} alt="Other Logo" />
+                <p className="pl-3 text-black">Class of 2020</p>
+              </div>
+              <div className="flex items-center ">
+                <Image src={DegreeLogo} alt="Other Logo" />
+                <p className="pl-3 text-black">MBA in Entrepreneurship</p>
+              </div>
+            </div>
+          </div>
+          <div className=" rounded-lg m-2 mb-12  shadow-lg">
+            {/* Profile Banner */}
+            <OffersAndAsk />
+          </div>
+        </div>
       </div>
       <div className="md:max-w-[600px] md:mr-10 md:pt-11 md:pb-14 md:overflow-y-scroll md:max-h-screen bg-gradient-to-b md:scrollbar-hide ">
         {/* Content for the right column */}
