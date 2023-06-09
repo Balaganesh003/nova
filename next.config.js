90097ada86714de90cd7186617996d629736dc1e
Balaganesh K <89638496+Balaganesh003@users.noreply.github.com>
2023-06-09 16:02:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['xsgames.co'],
-  },
-};
 
-module.exports = nextConfig;
